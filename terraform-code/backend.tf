2eb61fec2dea1ea566bd273b1ac3f7d9a6a705eb
nickngwa <130005168+nickngwa@users.noreply.github.com>
2025-03-07 14:08:29 -0500

    Update backend.tf

--- terraform-code/backend.tf
+++ terraform-code/backend.tf
@@ -1,28 +1,3 @@
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "nngwa-node-terraform-state"
-
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-
-}
-
 terraform {
   backend "s3" {
     bucket = "nngwa-node-terraform-state"
