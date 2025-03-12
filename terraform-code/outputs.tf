76dc261add2cf313b8fcd11ddb5d64659b7f9349
Nick Ngwa <nickngwa@nicks-mbp.mynetworksettings.com>
2025-03-12 12:16:11 -0400

    outputs

--- terraform-code/outputs.tf
+++ terraform-code/outputs.tf
@@ -2,3 +2,13 @@ output "db_endpoint" {
   description = "Database endpoint"
   value       = module.database.db_endpoint
 }
+
+output "web_endpoint" {
+  description = "Web tier load balancer endpoint"
+  value       = module.web_tier.web_endpoint
+}
+
+output "api_endpoint" {
+  description = "API tier load balancer endpoint"
+  value       = module.api_tier.api_endpoint
+}
